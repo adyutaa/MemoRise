41816bb8dfaffb7bd2c23470aa36d9dd695e5eca
Adyuta Indra Adyatma <adyuta24.hr@gmail.com>
2024-01-27 22:14:35 +0700

    added tab bar but isnt finished yet lol

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,7 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,6 +13,10 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
+    List<String> tabs = ["All Notes", "Folders"];
+
+    int current = 0;
+
     var size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
@@ -21,10 +28,43 @@ class _HomePageState extends State<HomePage> {
         child: Column(
           children: [
             Container(
-              child: TabBar(
-                tabs: [],
-              ),
-            )
+                height: size.height * 0.05,
+                width: size.width,
+                color: Colors.red,
+                child: Stack(
+                  children: [
+                    Container(
+                      width: size.width,
+                      height: size.height * 0.04,
+                      color: Colors.yellow,
+                      child: ListView.builder(
+                          scrollDirection: Axis.horizontal,
+                          itemCount: tabs.length,
+                          physics: BouncingScrollPhysics(),
+                          itemBuilder: (context, index) {
+                            return GestureDetector(
+                              onTap: () {
+                                setState(() {
+                                  current = index;
+                                });
+                              },
+                              child: Padding(
+                                padding: EdgeInsets.only(
+                                    left: index == 0 ? 120 : 40, top: 7),
+                                child: Text(
+                                  tabs[index],
+                                  style: GoogleFonts.poppins(
+                                      fontSize: current == index ? 16 : 14,
+                                      fontWeight: current == index
+                                          ? FontWeight.w500
+                                          : FontWeight.w400),
+                                ),
+                              ),
+                            );
+                          }),
+                    )
+                  ],
+                ))
           ],
         ),
       ),
@@ -35,7 +75,9 @@ class _HomePageState extends State<HomePage> {
           backgroundColor: Colors.white,
           shape:
               RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-          onPressed: () {},
+          onPressed: () {
+            setState(() {});
+          },
           label: Text("Add new notes"),
           icon: Icon(Icons.add),
         ),
